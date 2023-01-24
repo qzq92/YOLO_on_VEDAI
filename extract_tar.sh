5407793e1acb1478edd063b913355feed3f59fa9
Quek Zhi Qiang <quekzhiqiang92@hotmail.com>
2023-01-24 21:40:18 +0800

    Removed sections on the annotation files download as original annotations from source has formatting issue.

--- extract_tar.sh
+++ extract_tar.sh
@@ -13,15 +13,18 @@ annot_directory_to_check=./Annotations
 annot_tar=Annotations1024.tar
 
 # Download annotations
-wget https://downloads.greyc.fr/vedai/Annotations1024.tar
+#wget https://downloads.greyc.fr/vedai/Annotations1024.tar
 
 
+echo "Retrieving VEDAI images of 1024x1024 resolution online.."
 # Download higher resolution (1024x1024) images
 wget https://downloads.greyc.fr/vedai/Vehicules1024.tar.001
 wget https://downloads.greyc.fr/vedai/Vehicules1024.tar.002
 wget https://downloads.greyc.fr/vedai/Vehicules1024.tar.003
 wget https://downloads.greyc.fr/vedai/Vehicules1024.tar.004
 wget https://downloads.greyc.fr/vedai/Vehicules1024.tar.005
+echo "Download completed"
+
 
 if [ -d $veh_directory_to_check ] 
 then
@@ -30,19 +33,19 @@ then
 	echo "Completed"
 fi
 
-if [ -d $annot_directory_to_check ] 
-then
-    echo "Directory $annot_directory_to_check exists. Deleting and recreating for new extraction of the Vedai dataset annotations downloaded online."
-	rm -rf $annot_directory_to_check
-	echo "Completed"
-fi
+#if [ -d $annot_directory_to_check ] 
+#then
+#    echo "Directory $annot_directory_to_check exists. Deleting and recreating for new extraction of the Vedai dataset annotations downloaded online."
+#	rm -rf $annot_directory_to_check
+#	echo "Completed"
+#fi
 
 
-echo "Extracting annotation tar folder and renaming the folder..."
-tar -xvf Annotations1024.tar 
-mv Annotations1024 $annot_directory_to_check
-echo "Completed"
-echo ""
+#echo "Extracting annotation tar folder and renaming the folder..."
+#tar -xvf Annotations1024.tar 
+#mv Annotations1024 $annot_directory_to_check
+#echo "Completed"
+#echo ""
 
 echo "Extracting splitted tar folders and renaming the folder..."
 cat  Vehicules1024.tar.[0-9][0-9][0-9] | tar -xvf - 
