13ec0e6de6a0f71c18e8f9f2f3b32ce3a0571612
Quek Zhi Qiang <quekzhiqiang92@hotmail.com>
2023-01-26 21:20:42 +0800

    Updated without issues in execution

--- generate_train_val_files.sh
+++ generate_train_val_files.sh
@@ -7,7 +7,7 @@ set -e
 
 # Ratio of original data for validation purpose
 validation_ratio=0.1
-testing_ratio=0.1
+testing_ratio=0.2
 
 echo "Creating training and testing datasets with YOLO standard annotations. 'Train' and 'test' folders would be generated to store them..."
 python process_annotation_to_yolo.py --validation_ratio $validation_ratio --testing_ratio $testing_ratio
