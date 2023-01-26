810868d44ff794d0e5abb7585c0a633f7e089f1f
Quek Zhi Qiang <quekzhiqiang92@hotmail.com>
2023-01-26 20:59:13 +0800

    Updated script to allow configuration of proportion of data used for validation/testing

--- generate_train_val_files.sh
+++ generate_train_val_files.sh
@@ -5,9 +5,10 @@ PATH=$PWD:$PATH
 # Config to terminate on error
 set -e
 
-# Validation ratio for splitting
-validation_ratio=0.25
+# Ratio of original data for validation purpose
+validation_ratio=0.1
+testing_ratio=0.1
 
 echo "Creating training and testing datasets with YOLO standard annotations. 'Train' and 'test' folders would be generated to store them..."
-python process_annotation_to_yolo.py --validation_ratio $validation_ratio
+python process_annotation_to_yolo.py --validation_ratio $validation_ratio --testing_ratio $testing_ratio
 echo "Completed"
